0e1996e47ee3f755d198ae0e4311b511938e3938
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-08-27 23:19:40 +0530

    added minion api to program

--- app.js
+++ app.js
@@ -2,7 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#input");
 var outputText = document.querySelector("#output");
 
+var serverURL= "https://api.funtranslations.com/translate/minion.json";
+
+function translationText(text){
+
+   return serverURL+"?"+"text=" + text;
+}
+
+function errorhandler(error){
+
+   console.log("error occured", error);
+   alert("Please try after sometime server error");
+}
+
 function clickHandler() {
-   outputText.innerText= "jjdbafi" + textInput.value;
+   var inputText= textInput.value;
+   
+   fetch(translationText(inputText))
+   .then(response=>response.json())
+   .then(json => {
+      var translatedText = json.contents.translated;
+      outputText.innerText= translatedText;
+   })
+   .catch(errorhandler);
 };
 btnTranslate.addEventListener("click",clickHandler)
