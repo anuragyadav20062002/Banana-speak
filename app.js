63d2aba7bb6f6ae31377e6d367054d999dffc92a
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-08-27 13:04:08 +0530

    added textarea

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#input");
+
+console.log(input);
+
 var btnTranslate = addEventListener("click", function clickEventHandler(){
 console.log("clicked");
 })
